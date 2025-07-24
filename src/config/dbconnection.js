45431b2bd90286eabcba69a2cac3be3b142b1ade
Pedro M. <pedro.mugnol@gmail.com>
2025-07-23 21:07:32 -0300

    Add class for handling database connections

---
+++ src/config/dbconnection.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import {Sequelize} from 'sequelize';
+
+class DBConnection  {
+    #parametersDB
+    #connection
+    
+    constructor() {
+        this.#parametersDB = {
+            host: process.env.DB_HOST,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            dialect: 'mysql',
+        };
+        this.#connection = new Sequelize(this.#parametersDB);
+    }
+
+    async connect() {
+        try {
+            await this.#connection.authenticate();
+            console.log('Connection has been established');
+        } catch (error) {
+            console.log("Error in DB connection: ", error);
+        }
+    }
+
+    async close() {
+        await this.#connection.close()
+    }
+
+    get connection() {
+        return this.#connection;
+    }
+}
+
+export default DBConnection;

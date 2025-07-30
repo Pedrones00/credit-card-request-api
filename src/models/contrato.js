72b07f57887c840e658d4e363b29c78185d0fdd1
Pedro M. <pedro.mugnol@gmail.com>
2025-07-30 19:02:07 -0300

    Set default value for dt_inicio_vigencia and dt_fim_vigencia in Contrato model

--- src/models/contrato.js
+++ src/models/contrato.js
@@ -32,10 +32,16 @@ export default (sequelize) => {
             dt_inicio_vigencia: {
                 type: DataTypes.DATEONLY,
                 allowNull: false,
+                defaultValue: () => {
+                    let today = new Date();
+                    today.setHours(0, 0, 0, 0);
+                    return today;
+                },
             },
             dt_fim_vigencia: {
                 type: DataTypes.DATEONLY,
                 allowNull: false,
+                defaultValue: '9999-12-31',
             },
 
         },

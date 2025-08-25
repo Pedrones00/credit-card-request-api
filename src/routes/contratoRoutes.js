c131ee57493958b719662b7cc69c95052c533334
Pedro M. <pedro.mugnol@gmail.com>
2025-08-25 13:30:46 -0300

    fix: adjust contract-route endpoint behavior

--- src/routes/contratoRoutes.js
+++ src/routes/contratoRoutes.js
@@ -7,7 +7,7 @@ export default function contratoRouter(contratoController) {
     routes.get('/contratos/cadastrar', async (request, response) => contratoController.registerPage(request, response));
     routes.get('/contratos/visualizar/:id', (request, response) => contratoController.viewPage(request, response));
     routes.get('/contratos/editar/:id', async (request, response) => contratoController.editPage(request, response));
-    routes.get('/contrato/deletar/:id', async (request, response) => contratoController.deletePage(request, response));
+    routes.get('/contratos/deletar/:id', async (request, response) => contratoController.deletePage(request, response));
 
     routes.get('/api/contratos', async (request, response) => contratoController.listAllAPI(request, response));
     routes.get('/api/contratos/:id', async (request, response) => contratoController.searchIDAPI(request, response));

8794bcb34435990800f63fe0b3fd67aa7748f561
Pedro M. <pedro.mugnol@gmail.com>
2025-08-26 21:03:16 -0300

    docs: add API documentation with Swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import yaml from 'yamljs';
 import clienteRoute from './clienteRoutes.js';
 import cartaoRoute from './cartaoRoutes.js';
 import contratoRoute from './contratoRoutes.js';
@@ -17,9 +19,36 @@ const routes = async (app) => {
         return route;
     }
 
+    const docsRoutes = () => {
+        const route = express.Router();
+
+        const clientesDocs = yaml.load('src/docs/clientesDocs.yaml');
+        const cartoesDocs = yaml.load('src/docs/cartoesDocs.yaml');
+        const contratosDocs = yaml.load('src/docs/contratosDocs.yaml');
+
+        const docFiles = {
+            openapi: '3.0.0',
+            info: {
+                title: 'credit-card-request-api',
+                version: '0.0.1',
+                description: 'Card, Customer, and Contract Management API',
+                },
+            paths: {
+                ...clientesDocs.paths,
+                ...cartoesDocs.paths,
+                ...contratosDocs.paths,
+                }
+            };
+
+        route.use("/api/docs", swaggerUI.serve, swaggerUI.setup(docFiles, {explorer: false, docExpasion: 'true'}));
+        
+        return route;
+    }
+
     const routeNotFound = (request, response) => response.status(404).render("404", { titulo: "Página não encontrada" });
 
     app.use(indexRoute());
+    app.use(docsRoutes());
     app.use(clienteRoute(clienteController));
     app.use(cartaoRoute(cartaoController));
     app.use(contratoRoute(contratoController));

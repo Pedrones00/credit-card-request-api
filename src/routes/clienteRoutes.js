36d5a9a631d651bfd51cac8a4ae2fb45e8f35222
Pedro M. <pedro.mugnol@gmail.com>
2025-08-28 22:05:07 -0300

    style: fix indentation clienteRoute file

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -3,41 +3,19 @@ import express from "express";
 export default function clienteRoutes(clienteController) {
   const routes = express.Router();
 
-  routes.get("/clientes", async (request, response) =>
-    clienteController.indexPage(request, response)
-  );
-  routes.get("/clientes/cadastrar", async (request, response) =>
-    clienteController.registerPage(request, response)
-  );
-  routes.get("/clientes/visualizar/:id", async (request, response) =>
-    clienteController.viewPage(request, response)
-  );
-  routes.get("/clientes/editar/:id", async (request, response) =>
-    clienteController.editPage(request, response)
-  );
-  routes.get("/clientes/deletar/:id", async (request, response) =>
-    clienteController.deletePage(request, response)
-  );
+  routes.get("/clientes", async (request, response) => clienteController.indexPage(request, response));
+  routes.get("/clientes/cadastrar", async (request, response) => clienteController.registerPage(request, response));
+  routes.get("/clientes/visualizar/:id", async (request, response) => clienteController.viewPage(request, response));
+  routes.get("/clientes/editar/:id", async (request, response) => clienteController.editPage(request, response));
+  routes.get("/clientes/deletar/:id", async (request, response) => clienteController.deletePage(request, response));
 
-  routes.get("/api/clientes", async (request, response) =>
-    clienteController.listAllAPI(request, response)
-  );
-  routes.get("/api/clientes/:id", async (request, response) =>
-    clienteController.searchIDAPI(request, response)
-  );
+  routes.get("/api/clientes", async (request, response) => clienteController.listAllAPI(request, response));
+  routes.get("/api/clientes/:id", async (request, response) => clienteController.searchIDAPI(request, response));
 
-  routes.post("/api/clientes", async (request, response) =>
-    clienteController.registerAPI(request, response)
-  );
-  routes.patch("/api/clientes", async (request, response) =>
-    clienteController.updateClienteAPI(request, response)
-  );
-  routes.patch("/api/clientes/:id/activate", async (request, response) =>
-    clienteController.activateClienteAPI(request, response)
-  );
-  routes.delete("/api/clientes/:id", async (request, response) =>
-    clienteController.deleteClienteAPI(request, response)
-  );
+  routes.post("/api/clientes", async (request, response) => clienteController.registerAPI(request, response));
+  routes.patch("/api/clientes", async (request, response) => clienteController.updateClienteAPI(request, response));
+  routes.patch("/api/clientes/:id/activate", async (request, response) => clienteController.activateClienteAPI(request, response));
+  routes.delete("/api/clientes/:id", async (request, response) => clienteController.deleteClienteAPI(request, response));
 
   return routes;
 }
